7b4673168c8796a2b45b8737b8a60c5eca169583
Haseeb702 <80732008+Haseeb702@users.noreply.github.com>
2024-09-06 14:09:15 -0400

    auth/added

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCHcau2hu0KlVVBCD3FwCffyNL_2lf6VOw",
+  authDomain: "personal-dashboard-appli-b5862.firebaseapp.com",
+  projectId: "personal-dashboard-appli-b5862",
+  storageBucket: "personal-dashboard-appli-b5862.appspot.com",
+  messagingSenderId: "1019905844822",
+  appId: "1:1019905844822:web:6b24cfa849e8ee79f17df7",
+  measurementId: "G-MMX11TD4VY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//export const;
